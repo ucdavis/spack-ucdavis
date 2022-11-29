ec84def1fb4ae4a445500128f9512612b4e6dd02
Camille Scott <cswel@ucdavis.edu>
2022-11-29 14:12:31 -0800

    r-annotatr: bioc, use commit hash

--- repos/hpccf/packages/r-annotatr/package.py
+++ repos/hpccf/packages/r-annotatr/package.py
@@ -3,42 +3,16 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install r-annotatr
-#
-# You can edit this file again by typing:
-#
-#     spack edit r-annotatr
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack.package import *
 
 
 class RAnnotatr(RPackage):
     """Annotation of Genomic Regions to Genomic Annotations"""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://bioconductor.org/packages/release/bioc/html/annotatr.html"
-    url = "https://bioconductor.org/packages/release/bioc/src/contrib/annotatr_1.22.0.tar.gz"
     bioc = "annotatr"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ["github_user1", "github_user2"]
+    version("1.22.0", commit="dde0987196966621e3dfe882b228248a3e62d8bc")
 
-    version("1.22.0", sha256="a0e20bf8a9cfa2ca2913b3df56f928ee1f4edc92ce0fc7296206913676ea6ab9")
-
-    # FIXME: Add dependencies if required.
-    # depends_on("r-foo", type=("build", "run"))
     depends_on("r-annotationdbi", type=("build", "run"))
     depends_on("r-annotationhub", type=("build", "run"))
     depends_on("r-dplyr", type=("build", "run"))
