14850f23c45db98a736545aab34290404f83817e
Camille Scott <cswel@ucdavis.edu>
2022-12-02 11:39:50 -0800

    Reorganize templates directory have cluster-specific subdirs

---
+++ templates/hpccf/franklin/r-stack-view.tcl
@@ -0,0 +1,19 @@
+{% extends "modules/modulefile.tcl" %}
+{% block footer %}
+
+setenv R_VIEW_HOME /share/apps/spack/environments/r-stack/views/{{ spec.version }}
+setenv R_HOME $env(R_VIEW_HOME)/rlib/R/
+setenv R_ROOT $env(R_VIEW_HOME)
+
+prepend-path PATH $env(R_VIEW_HOME)/bin
+prepend-path LD_LIBRARY_PATH $env(R_VIEW_HOME)/rlib/R/lib
+prepend-path PKG_CONFIG_PATH $env(R_VIEW_HOME)/rlib/pkgconfig
+prepend-path CMAKE_PREFIX_PATH $env(R_VIEW_HOME)
+
+prepend-path R_LIBS $env(R_VIEW_HOME)/rlib/R/library
+prepend-path R_LIBS /share/apps/spack/Rlibs/{{ spec.version }}
+
+if [module-info mode load] {
+   puts stderr "Loaded R version {{ spec.version }} and libraries."
+}
+{% endblock %}
