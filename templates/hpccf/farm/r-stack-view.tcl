46c851a73764c7eb870e04d55cfbf7289dff1f02
Camille Scott <cswel@ucdavis.edu>
2022-12-06 11:20:40 -0800

    OW: export R_LIBS_USER and mkdir it for the user.

--- templates/hpccf/farm/r-stack-view.tcl
+++ templates/hpccf/farm/r-stack-view.tcl
@@ -13,6 +13,11 @@ prepend-path CMAKE_PREFIX_PATH $env(R_VIEW_HOME)
 prepend-path R_LIBS_SITE $env(R_VIEW_HOME)/rlib/R/library
 prepend-path R_LIBS_SITE /share/apps/spackuser/Rlibs/{{ spec.version }}
 
+set RMajorVersion [ string range {{ spec.version }} 0 end-2 ]
+
+prepend-path R_LIBS_USER $env(HOME)/R/%p-library/%v
+file mkdir $env(HOME)/R/x86_64-pc-linux-gnu-library/$RMajorVersion
+
 if [module-info mode load] {
    puts stderr "Loaded R version {{ spec.version }} and libraries."
 }
