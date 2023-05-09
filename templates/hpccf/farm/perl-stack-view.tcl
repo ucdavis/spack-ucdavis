2d1ff35a19da30e0c4511e8253220b59676eb1a7
Omen Wild <omen@ucdavis.edu>
2023-05-09 10:40:03 -0700

    Set environment so well behaved Perl modules can be user installed.

--- templates/hpccf/farm/perl-stack-view.tcl
+++ templates/hpccf/farm/perl-stack-view.tcl
@@ -7,6 +7,12 @@ setenv PERL_ROOT $env(PERL_VIEW_HOME)/bin
 prepend-path PERL5LIB $env(PERL_VIEW_HOME)/lib
 prepend-path PERL5LIB $env(PERL_VIEW_HOME)/lib/perl5
 
+# Set environment so well behaved Perl modules can be user installed.
+setenv PERL_MM_OPT INSTALL_BASE=/home/$env(LOGNAME)/perl5
+prepend-path PERL5LIB /home/$env(LOGNAME)/perl5/lib/perl5
+prepend-path PATH     /home/$env(LOGNAME)/perl5/bin
+prepend-path MANPATH  /home/$env(LOGNAME)/perl5/man
+
 prepend-path PATH    $env(PERL_VIEW_HOME)/bin
 prepend-path MANPATH $env(PERL_VIEW_HOME)/man
 prepend-path CMAKE_PREFIX_PATH $env(PERL_VIEW_HOME)/.
