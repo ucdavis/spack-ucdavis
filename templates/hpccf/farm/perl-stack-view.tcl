0cd8405d0e1393c36a626028f7586aac06ad2d81
Omen Wild <omen@ucdavis.edu>
2023-05-09 17:20:59 -0700

    Update the module file to use the output of local::lib to get cpan modules to install per-user.

--- templates/hpccf/farm/perl-stack-view.tcl
+++ templates/hpccf/farm/perl-stack-view.tcl
@@ -8,10 +8,14 @@ prepend-path PERL5LIB $env(PERL_VIEW_HOME)/lib
 prepend-path PERL5LIB $env(PERL_VIEW_HOME)/lib/perl5
 
 # Set environment so well behaved Perl modules can be user installed.
-setenv PERL_MM_OPT INSTALL_BASE=/home/$env(LOGNAME)/perl5
-prepend-path PERL5LIB /home/$env(LOGNAME)/perl5/lib/perl5
-prepend-path PATH     /home/$env(LOGNAME)/perl5/bin
-prepend-path MANPATH  /home/$env(LOGNAME)/perl5/man
+# From: perl -Mlocal::lib
+
+setenv PERL_LOCAL_LIB_ROOT /home/$env(LOGNAME)/perl5
+
+prepend-path PATH          $env(PERL_LOCAL_LIB_ROOT)/bin
+setenv       PERL5LIB      $env(PERL_LOCAL_LIB_ROOT)/lib/perl5
+setenv       PERL_MM_OPT   INSTALL_BASE=$env(PERL_LOCAL_LIB_ROOT)
+setenv       PERL_MB_OPT   "--install_base $env(PERL_LOCAL_LIB_ROOT)"
 
 prepend-path PATH    $env(PERL_VIEW_HOME)/bin
 prepend-path MANPATH $env(PERL_VIEW_HOME)/man
