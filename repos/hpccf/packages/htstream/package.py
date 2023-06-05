b47d66638fc8e14e93f656a56306bb9c7afbac58
Omen Wild <omen@ucdavis.edu>
2023-06-05 16:06:33 -0700

    Snapshot of modified packages.

---
+++ repos/hpccf/packages/htstream/package.py
@@ -0,0 +1,46 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install htstream
+#
+# You can edit this file again by typing:
+#
+#     spack edit htstream
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack.package import *
+
+
+class Htstream(CMakePackage):
+    """FIXME: Put a proper description of your package here."""
+
+    # FIXME: Add a proper url for your package's homepage here.
+    homepage = "https://www.example.com"
+    url = "https://github.com/s4hts/HTStream/archive/refs/tags/v1.3.3.tar.gz"
+
+    # FIXME: Add a list of GitHub accounts to
+    # notify when the package is updated.
+    # maintainers = ["github_user1", "github_user2"]
+
+    version("1.3.3", sha256="1cccafd926615877a8b48cdd8efc118ad90289b378d86fd9c3c8368011419f4c")
+
+    depends_on("boost@1.56:")
+
+    def cmake_args(self):
+        # FIXME: Add arguments other than
+        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
+        # FIXME: If not needed delete this function
+        args = []
+        return args
