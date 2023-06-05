55d2194089fda65a4478b63402b47479a2789e3b
Omen Wild <omen@ucdavis.edu>
2023-06-05 11:59:43 -0700

    Custom module template for turbovnc to print our Farm/local instructions for how to run it on the cluster.

---
+++ templates/hpccf/farm/turbovnc.tcl
@@ -0,0 +1,39 @@
+{% extends "hpccf/default.tcl" %}
+{% block footer %}
+{{ super() }}
+
+setenv TURBOVNC_SERVER_UTILS /share/apps/22.04/hpccf-wrappers/turbovnc
+prepend-path PATH $env(TURBOVNC_SERVER_UTILS)
+
+module load virtualgl
+
+if { [module-info mode load] } {
+
+   set hostname [exec /bin/hostname]
+
+   if {[string equal $hostname "farm"]} {
+      puts stderr "
+TurboVNC is not allowed on the head node.
+
+You need to run it on a node through srun and use SSH to do port forwarding.
+
+For example: srun --partition=gpu-a100-h --gres=gpu:1 --time=9:00:00 --ntasks=1 --cpus-per-task=4 --mem=50G --pty /bin/bash -l
+
+After your srun has been allocated resources you can then: module load [module-info name]
+
+The wrapper script will automatically run, but you can rerun it if necessary: turbovnc-farm
+"
+      exit
+   }
+
+   puts stderr "
+
+Automatically running the wrapper, you may rerun it if necessary: turbovnc-farm
+
+"
+   
+   puts stdout "turbovnc-farm"
+
+}
+
+{% endblock %}
