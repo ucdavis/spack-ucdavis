f3b94957a2c53e234f4b11dcfe3372f4d35ee459
Camille Scott <cswel@ucdavis.edu>
2024-03-07 14:09:35 -0800

    [package] rstudio: bring over farm 2023.12.1-402 updates

--- repos/forked/packages/rstudio/package.py
+++ repos/forked/packages/rstudio/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -14,6 +14,9 @@ class Rstudio(CMakePackage):
 
     homepage = "www.rstudio.com/products/rstudio/"
 
+    version("2023.12.1-402",
+            sha256="196d31094d580a74737fbf689d2d0b302da5fec13694aa1d63f8875d3e45e4dd")
+
     version("2022.12.0-353",
             sha256="e4f3503e2ad4229301360f56fd5288e5c8e769c490073dae7fe40366237ecce0")
 
@@ -21,8 +24,13 @@ class Rstudio(CMakePackage):
             url="https://github.com/rstudio/rstudio/archive/refs/tags/v1.4.1717.tar.gz",
             sha256="3af234180fd7cef451aef40faac2c7b52860f14a322244c1c7aede029814d261")
 
+    # https://github.com/rstudio/rstudio/archive/refs/tags/2023.12.1+402.tar.gz
     def url_for_version(self, version):
-        url = "https://github.com/rstudio/rstudio/archive/refs/tags/v{0}.tar.gz"
+        if version >= Version("2023.12.1"):
+            url = "https://github.com/rstudio/rstudio/archive/refs/tags/{0}.tar.gz"
+        else:
+            url = "https://github.com/rstudio/rstudio/archive/refs/tags/v{0}.tar.gz"
+        
         return url.format(str(version).replace('-', '+'))
 
     variant("notebook", default=False, description="Enable notebook support.")
@@ -32,12 +40,10 @@ class Rstudio(CMakePackage):
     depends_on("cmake@3.4.3:", type="build")
     depends_on("pkgconfig", type="build")
     depends_on("ant", type="build")
-    depends_on("{0}+pic".format(Boost.with_default_variants))
     #depends_on("qt+webkit@5.12:5.14")
     depends_on("qt@5.15.8:")
     depends_on("patchelf@0.9:")
     depends_on("yaml-cpp@:0.6.3")  # find_package fails with newest version
-    depends_on("node-js@16:")
     depends_on("yarn")
     depends_on("pandoc@2.11.4:")
     depends_on("icu4c")
@@ -47,6 +53,26 @@ class Rstudio(CMakePackage):
     depends_on("openssl@3:")
     depends_on("gettext")
 
+    # depends_on("{0}+pic".format(Boost.with_default_variants))
+    # 48      Could NOT find Boost: Found unsuitable version "1.80.0", but required is at
+    # 49      least "1.83.0" (found
+
+
+    # require EXACT node version to match rstudio expectation
+    with when("@2022.12.0"):
+        depends_on("{0}@:1.82.9999+pic".format(Boost.with_default_variants))
+        depends_on("node-js@16:")
+        depends_on("yarn@1.22.4:")
+        depends_on("npm@9.8.1")
+
+    # require EXACT node version to match rstudio expectation
+    with when("@2023.12.1-402"):
+        depends_on("{0}@1.83.0:+pic".format(Boost.with_default_variants))
+        depends_on("node-js@18.18.2")
+        depends_on("yarn@1.22.4:")
+        depends_on("npm@9.8.1")
+        depends_on("openssl@3.2:")
+    
     with when("+notebook"):
         depends_on("r-base64enc")
         depends_on("r-digest")
@@ -66,9 +92,7 @@ class Rstudio(CMakePackage):
         depends_on("r-xfun")
         depends_on("r-yaml")
 
-    # to use node-js provided by spack: first patches this portion of config to a single line
-    patch("0004-use-system-node.patch")
-
+        
     def cmake_args(self):
 
         args = [
@@ -97,13 +121,34 @@ class Rstudio(CMakePackage):
         env.set("RSTUDIO_TOOLS_ROOT", self.prefix.tools)
 
     def patch(self):
-        # Now patch that single line for node to the spack provided version
-        filter_file(
-            '<property name="node.bin" value="/usr/bin/node"/>',
-            '<property name="node.bin" value="{0}"/>'.format(self.spec["node-js"].prefix.bin.node),
-            "src/gwt/build.xml",
-            string=True,
-        )
+        if self.spec.satisfies("@2022.12.0:"):
+            filter_file(
+                '<property name="node.dir" value="../../dependencies/common/node/${node.version}"/>',
+                '<property name="node.dir" value="{0}"/>'.format(self.spec["node-js"].prefix),
+                "src/gwt/build.xml",
+                string=True,
+            )
+
+        if self.spec.satisfies("@2022.12.0-353"):
+            filter_file(
+                '<condition property="node.bin" value="../../../${node.dir}/bin/node">',
+                '<condition property="node.bin" value="${node.dir}/bin/node">',
+                "src/gwt/build.xml",
+                string=True,
+            )
+
+            
+        else:
+            # to use node-js provided by spack: first patches this portion of config to a single line
+            patch("0004-use-system-node.patch")
+
+            # Now patch that single line for node to the spack provided version
+            filter_file(
+                '<property name="node.bin" value="/usr/bin/node"/>',
+                '<property name="node.bin" value="{0}"/>'.format(self.spec["node-js"].prefix.bin.node),
+                "src/gwt/build.xml",
+                string=True,
+            )
 
         # remove hardcoded soci path to use spack soci
         if self.spec["soci"].version <= Version("4.0.0"):
@@ -132,6 +177,10 @@ class Rstudio(CMakePackage):
         deps = Executable("./dependencies/common/install-mathjax")
         deps()
 
+        if self.spec.satisfies("@2023.12.1:"):
+            deps = Executable("./dependencies/common/install-panmirror")
+            deps()
+            
         # two methods for pandoc
         # 1) replace install-pandoc:
         #    - link pandoc into tools/pandoc/$PANDOC_VERSION
@@ -168,3 +217,13 @@ class Rstudio(CMakePackage):
             os.symlink(
                 os.path.join(self.spec["pandoc"].prefix.bin, "pandoc-citeproc"), "pandoc-citeproc"
             )
+
+        if self.spec.satisfies("@2022.12.0:"):
+            node_shim_dir = f"dependencies/common/node/{self.spec['node-js'].version}/bin"
+            os.makedirs(node_shim_dir)
+            with working_dir(node_shim_dir):
+                os.symlink(self.spec["node-js"].prefix.bin.node, "node")
+                os.symlink(self.spec["node-js"].prefix.bin.corepack, "corepack")
+                os.symlink(self.spec["yarn"].prefix.bin.yarn, "yarn")
+                os.symlink(self.spec["npm"].prefix.bin.npm, "npm")
+                os.symlink(self.spec["npm"].prefix.bin.npx, "npx")
