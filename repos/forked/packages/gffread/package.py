acb2d9c89b6a9c09858df8c08d3b63ee62fc2c1c
Camille Scott <cswel@ucdavis.edu>
2024-03-07 01:55:23 -0800

    [package] gffread: update to new-style maintainers directive

--- repos/forked/packages/gffread/package.py
+++ repos/forked/packages/gffread/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -14,7 +14,7 @@ class Gffread(MakefilePackage):
     homepage = "https://github.com/gpertea/gffread"
     url = "https://github.com/gpertea/gffread/archive/refs/tags/v0.12.7.tar.gz"
 
-    maintainers = ["camillescott"]
+    maintainers("camillescott")
 
     version("0.12.7", sha256="6f3417090b3beb74f435bf5d462c87475b3c704a8a0d71821f6c1321c47dbbbe")
 
