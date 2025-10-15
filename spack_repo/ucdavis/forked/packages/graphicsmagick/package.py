2b6437ad16f769a369db4d5b00e9e857ab2d8bda
Software User <hpc-help@ucdavis.edu>
2025-10-15 12:20:06 -0700

    graphicsmafick: merge v1

--- spack_repo/ucdavis/forked/packages/graphicsmagick/package.py
+++ spack_repo/ucdavis/forked/packages/graphicsmagick/package.py
@@ -3,6 +3,8 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+from spack_repo.builtin.build_systems.autotools import AutotoolsPackage
+
 from spack.package import *
 
 
@@ -18,6 +20,8 @@ class Graphicsmagick(AutotoolsPackage):
     homepage = "http://www.graphicsmagick.org/"
     url = "https://sourceforge.net/projects/graphicsmagick/files/graphicsmagick/1.3.29/GraphicsMagick-1.3.29.tar.xz/download"
 
+    license("MIT")
+
     version("1.3.43", sha256="2b88580732cd7e409d9e22c6116238bef4ae06fcda11451bf33d259f9cbf399f")
     version("1.3.34", sha256="df009d5173ed0d6a0c6457234256c5a8aeaace782afa1cbab015d5a12bd4f7a4")
     version("1.3.33", sha256="130cb330a633580b5124eba5c125bbcbc484298423a97b9bed37ccd50d6dc778")
@@ -30,7 +34,7 @@ class Graphicsmagick(AutotoolsPackage):
     depends_on("cxx", type="build")  # generated
 
     depends_on("bzip2")
-    depends_on("ghostscript")
+    depends_on("ghostscript+gtk")
     depends_on("ghostscript-fonts")
     depends_on("graphviz")
     depends_on("jasper")
