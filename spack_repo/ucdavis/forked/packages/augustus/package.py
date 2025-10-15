36ced4fb84eb1d50798d69a17418f47e9cf2b2a4
Software User <hpc-help@ucdavis.edu>
2025-10-15 13:48:56 -0700

    forked.augustuc merge

--- spack_repo/ucdavis/forked/packages/augustus/package.py
+++ spack_repo/ucdavis/forked/packages/augustus/package.py
@@ -5,8 +5,10 @@
 
 import glob
 
+from spack_repo.builtin.build_systems.makefile import MakefilePackage
+from spack_repo.builtin.packages.boost.package import Boost
+
 from spack.package import *
-from spack.pkg.builtin.boost import Boost
 
 
 class Augustus(MakefilePackage):
@@ -175,7 +177,7 @@ class Augustus(MakefilePackage):
             files = glob.glob("*.py")
             filter_file(pattern, repl, *files, backup=False)
 
-    def setup_build_environment(self, env):
+    def setup_build_environment(self, env: EnvironmentModifications) -> None:
         htslib = self.spec["htslib"].prefix
         bamtools = self.spec["bamtools"].prefix
 
@@ -186,6 +188,6 @@ class Augustus(MakefilePackage):
             env.set("HTSLIB_INSTALL_DIR", htslib)
             env.set("BAMTOOLS_INSTALL_DIR", bamtools)
 
-    def setup_run_environment(self, env):
+    def setup_run_environment(self, env: EnvironmentModifications) -> None:
         env.set("AUGUSTUS_CONFIG_PATH", join_path(self.prefix, "config"))
         env.prepend_path("PATH", join_path(self.prefix, "scripts"))
